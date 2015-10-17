1a41472971f1982279872ce87e9c0852dff2764b
Rogermax <rogermoreta@gmail.com>
2015-10-17 13:31:51 +0200

    Ya guardamos el mapa logico, y muchas mas cosas, ahora toca pintar el mapa, i settear el tamaño del mapa a partir del mapa cofificado, y decirle a los bichos que sigan el camino, y al constructor que contruya solo en cesped.

--- app/src/main/java/com/gmail/rogermoreta/speedpaintmaze/model/BaseMonster.java
+++ app/src/main/java/com/gmail/rogermoreta/speedpaintmaze/model/BaseMonster.java
@@ -1,9 +1,9 @@
 package com.gmail.rogermoreta.speedpaintmaze.model;
 
 public class BaseMonster extends Enemy {
-    public BaseMonster() {
-        super();
-        posX = 1;
-        posY= 1;
-    }
+    public static final int pointOfDamageX = 18;
+    public static final int pointOfDamageY = 15;
+    public static final int radius = 6;
+    public static final int originalSize = 32;
+
 }
